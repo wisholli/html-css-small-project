0e531eb47b5d06458d9bb5a9bd07c0144e9287a7
wisholli <osribna4@gmail.com>
2023-04-24 16:05:55 +0300

    first commit

---
+++ project/js/app.js
@@ -0,0 +1,25 @@
+const getElement = (selector) => {
+  let element = document.querySelector(selector);
+  if (element) return element;
+  throw Error(
+    `Please double check your class names, there is no ${selector} class`
+  );
+};
+
+const links = getElement(".nav-links");
+const navBtnDOM = getElement(".nav-btn");
+
+navBtnDOM.addEventListener("click", () => {
+  links.classList.toggle("show-links");
+});
+
+const date = getElement(".date");
+const currentYear = new Date().getFullYear();
+date.textContent = currentYear;
+
+const recipesBtnDOM = getElement(".tags-btn");
+const tags = getElement(".tags-list");
+
+recipesBtnDOM.addEventListener("click", () => {
+  tags.classList.toggle("show-tags");
+});
